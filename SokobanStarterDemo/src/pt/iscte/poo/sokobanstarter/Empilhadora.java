96aa9696d76890ac7ec272a55e9c490b040d5d8b
Orizuro <alexandre.plotean@gmail.com>
2023-11-14 10:01:06 +0000

    init

---
+++ SokobanStarterDemo/src/pt/iscte/poo/sokobanstarter/Empilhadora.java
@@ -0,0 +1,82 @@
+package pt.iscte.poo.sokobanstarter;
+
+import java.util.Random;
+
+import pt.iscte.poo.gui.ImageTile;
+import pt.iscte.poo.utils.Direction;
+import pt.iscte.poo.utils.Point2D;
+
+public class Empilhadora implements ImageTile{
+
+	private Point2D position;
+	private String imageName;
+	
+	public Empilhadora(Point2D initialPosition){
+		position = initialPosition;
+		imageName = "Empilhadora_D";
+	}
+	
+	@Override
+	public String getName() {
+		return imageName;
+	}
+
+	@Override
+	public Point2D getPosition() {
+		return position;
+	}
+
+	@Override
+	public int getLayer() {
+		return 2;
+	}
+	public void moveDown() {
+	    Point2D newPosition = position.plus(Direction.DOWN.asVector());
+	    if (isValidPosition(newPosition)) {
+	        position = newPosition;
+	    }
+	}
+
+	public void moveLeft() {
+	    Point2D newPosition = position.plus(Direction.LEFT.asVector());
+	    if (isValidPosition(newPosition)) {
+	        position = newPosition;
+	    }
+	}
+
+	public void moveRight() {
+	    Point2D newPosition = position.plus(Direction.RIGHT.asVector());
+	    if (isValidPosition(newPosition)) {
+	        position = newPosition;
+	    }
+	}
+	public void moveUP() {
+	    Point2D newPosition = position.plus(Direction.UP.asVector());
+	    if (isValidPosition(newPosition)) {
+	        position = newPosition;
+	    }
+	}
+
+	private boolean isValidPosition(Point2D newPosition) {
+	    return newPosition.getX() >= 0 && newPosition.getX() < 10 &&
+	           newPosition.getY() >= 0 && newPosition.getY() < 10;
+	}
+	
+	
+	
+	public void move() {
+		
+		// Gera uma direcao aleatoria para o movimento
+		Direction[] possibleDirections = Direction.values();
+		Random randomizer = new Random();
+		int randomNumber = randomizer.nextInt(possibleDirections.length);
+		Direction randomDirection = possibleDirections[randomNumber];
+		
+		// Move segundo a direcao gerada, mas so' se estiver dentro dos limites
+		Point2D newPosition = position.plus(randomDirection.asVector());
+		if (newPosition.getX()>=0 && newPosition.getX()<10 && 
+			newPosition.getY()>=0 && newPosition.getY()<10 ){
+			position = newPosition;
+		}
+	}
+}
